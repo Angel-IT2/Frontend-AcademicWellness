42d04152eab95daee205289f9b05ff998223b402
Angel Maluleke <135185608+Angel-IT2@users.noreply.github.com>
2025-09-13 19:41:20 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import "./App.css";
 // Components
 import Header from "./components/sectionComponents/header";
 import Footer from "./components/sectionComponents/footer";
+
+// Existing Pages
 import Home from "./components/pageComponents/Home";
 import Login from "./components/pageComponents/Login";
 import Register from "./components/pageComponents/Register";
 import WhatsTheDifference from "./components/pageComponents/WhatsTheDifference";
 import FAQ from "./components/pageComponents/FAQ";
+
+// New Pages
+import TwoWeekPlanner from "./components/pageComponents/TwoWeekPlanner";
+import MonthlyPlanner from "./components/pageComponents/MonthlyPlanner";
+import AcademicChatboxes from "./components/pageComponents/AcademicChatboxes";
+import DashboardLayout from "./components/pageComponents/DashboardLayout"; // <-- Added
 
 function App() {
   return (
@@ -18,11 +26,20 @@ function App() {
         <Header />
         <div className="body">
           <Routes>
+            {/* Existing Routes */}
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/WhatsTheDifference" element={<WhatsTheDifference />} />
+            <Route path="/whats-the-difference" element={<WhatsTheDifference />} />
             <Route path="/faqs" element={<FAQ />} />
+
+            {/* Dashboard Route */}
+            <Route path="/dashboard" element={<DashboardLayout />} />
+
+            {/* New Routes */}
+            <Route path="/two-week-planner" element={<TwoWeekPlanner />} />
+            <Route path="/monthly-planner" element={<MonthlyPlanner />} />
+            <Route path="/academic-chatboxes" element={<AcademicChatboxes />} />
           </Routes>
         </div>
         <Footer />
