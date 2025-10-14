a73dddfb689ae296e71db0fa279f470f773c71c1
Angel Maluleke <135185608+Angel-IT2@users.noreply.github.com>
2025-10-14 17:46:32 +0200

    Update api.js

--- src/components/apiComponents/api.js
+++ src/components/apiComponents/api.js
@@ -1,7 +1,10 @@
-export const API_URL = "https://backend-academicwellness.onrender.com";
+export const API_URL = "https://backend-academicwellness.onrender.com/api";
 
+// Get auth headers with Bearer token
 export const getAuthHeaders = () => {
-  const token = localStorage.getItem("token");
+  const user = JSON.parse(localStorage.getItem("user"));
+  const token = localStorage.getItem("token") || user?.access;
+
   if (!token) return {};
   return {
     Authorization: `Bearer ${token}`,
