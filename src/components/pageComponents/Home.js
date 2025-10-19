3fa9660b00f58938e3853b4a00adee8625c8d407
Angel Maluleke <135185608+Angel-IT2@users.noreply.github.com>
2025-10-19 14:18:25 +0200

    Update Home.js

--- src/components/pageComponents/Home.js
+++ src/components/pageComponents/Home.js
@@ -1,9 +1,11 @@
 import "./Home.css";
 import Navbar from "./Navbar";
+import GoogleAnalytics from "./GoogleAnalytics"; // import GA component
 
 function Home() {
   return (
     <div className="home-card">
+      <GoogleAnalytics /> {/* Tracks pageviews automatically */}
       <Navbar />
       <div className="home-content">
         <h1 className="welcome-text">Welcome to UniPath</h1>
